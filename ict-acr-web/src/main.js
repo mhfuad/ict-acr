dcd43063b678b7849b5d564b874e867bf2f0abd9
mhfuad <mhfuad2@gmail.com>
2023-05-28 18:40:46 +0600

    bootstrap-vue added

--- ict-acr-web/src/main.js
+++ ict-acr-web/src/main.js
@@ -3,9 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
+app.use(BootstrapVue3)
 
 app.mount('#app')
