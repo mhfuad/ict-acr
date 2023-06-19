1fb636348c8b3cf1e70456e28c855f27aead7f3f
mhfuad <mhfuad2@gmail.com>
2023-06-19 19:32:46 +0600

    form crud done

--- ict-acr-api/firstClass/src/index.js
+++ ict-acr-api/firstClass/src/index.js
@@ -3,9 +3,13 @@ const express = require('express')
 const app = express();
 app.use(express.json());
 
+const formRoute = require('../routes/formRoutes')
+
 app.get("/", (req, res) => {
     res.send({"msg":"Hello from firstClass"});
 });
+
+app.use('/form', formRoute)
 
 app.listen(8001, () => {
     console.log(`firstClass is running on port 8001`)
