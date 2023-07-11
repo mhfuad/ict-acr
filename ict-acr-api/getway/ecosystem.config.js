92299b1de53f9b830ea867a8f94094b0d8fdc7eb
mhfuad <mhfuad2@gmail.com>
2023-07-11 10:48:45 +0600

    local change

--- ict-acr-api/getway/ecosystem.config.js
+++ ict-acr-api/getway/ecosystem.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   apps: [
     {
       name: 'ict-acr-api',
@@ -14,20 +13,3 @@ module.exports = {
     }
   ]
 }
-=======
-    apps: [
-      {
-        name: 'ict-acr-api',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './src/index.js',
-        args: 'start',
-        env: {
-            NODE_ENV: "production",
-            PORT: 8000,
-        }
-      }
-    ]
-  }
-  
->>>>>>> 166d698ffbdbfe08f0cd455693f8ee6d17e10ba8
