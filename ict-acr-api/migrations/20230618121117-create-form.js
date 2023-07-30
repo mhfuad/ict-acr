b3d8eadb083715fd8ef89069923b65ccccb1775b
mhfuad <mhfuad2@gmail.com>
2023-07-30 17:28:17 +0600

    form status

--- ict-acr-api/migrations/20230618121117-create-form.js
+++ ict-acr-api/migrations/20230618121117-create-form.js
@@ -64,7 +64,7 @@ module.exports = {
       },
       status:{
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: true
       },
       createdAt: {
         allowNull: false,
